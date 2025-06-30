7dfeb6131fb29ac778e15576f114daae15d609eb
Bruno Bueno <128551954+BrunooBuenoo@users.noreply.github.com>
2025-06-30 19:21:00 -0300

    Update Patrocinador

--- src/components/PatrocinadorModal.vue
+++ src/components/PatrocinadorModal.vue
@@ -800,7 +800,7 @@ export default {
 @media (max-width: 480px) {
   .modal-overlay {
     padding: 0.25rem;
-    padding-top: 0.3rem;
+    padding-top: 0.15rem;
     padding-bottom: 3rem;
     
   }
